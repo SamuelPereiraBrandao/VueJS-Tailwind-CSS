64d05109ac51ab480911cb56c2e360caa01507df
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-29 19:27:30 -0300

    add

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class=" flex  justify-center">
-
+    <div class=" flex  justify-center items-center">
+<!--  h-screen w-screen -->
   
     <div class="p-2 max-w-md bg-gray-200 rounded shadow-md dark:bg-blue-800 dark:text-white">
         <h2 class="font-bold text-center text-lg p-3">Login</h2>
@@ -12,19 +12,19 @@
             <div>
                  <label for="senha" class="text-sm">Senha: </label>
             <input type="text" id="senha" class="rounded mb-2 w-80 bg-gray-50 border border-gray-300 text-gray-800 placeholder-gray-300 focus:outline-none">
-            <a href="" class="text-sm hover:text-blue-400 transition-all">esqueci minha senha</a>
+            <a href="" class="text-sm hover:text-blue-400 transition-all text-right">esqueci minha senha</a>
             </div> 
             <div>
                 <button class=" w-full dark:bg-blue-600 bg-blue-400 font-bold p-2 text-white rounded mb-2  ">Entrar</button>
             </div>
            
         </div>
-        <div class=" flex items-center justify-center text-center dark:hidden text-black">
+<!--         <div class=" flex items-center justify-center text-center dark:hidden text-black">
             <h1>Modo white</h1>
         </div>
         <div class="dark:visible invisible text-white">
             <h1>Modo dark</h1>
-        </div>
+        </div> -->
     </div>
 </div>
 </template>
