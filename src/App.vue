8f21341291f56407553cba0e5239b06a2deb7266
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-29 19:30:12 -0300

    add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav >
 
-    <router-link class="text-black hover:text-green-500" to="/">Home</router-link> |
+    <router-link  to="/">Home</router-link> |
     <router-link to="/about">About</router-link> | 
     <router-link to="/login">Login</router-link>
   </nav>
